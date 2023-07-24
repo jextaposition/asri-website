5e605f06a3f28ed28316a1e0d4328d0f4786ca57
Alexa Hayes <jextaposition@Alexas-iMac.attlocal.net>
2023-07-24 12:09:25 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/font' element={<Fonts />} />
+        </Routes>
+      </BrowserRouter>
     </div>
+  );
+}
+
+function Fonts() {
+  return (
+    <di>
+      <h2 className='apoc'>Apoc</h2>
+      <h2 className='buda-light'>Buda-Light</h2>
+      <h2 className='bw'>Bw Gradual DEMO ExtraBold</h2>
+      <h2 className='carlton'>Carlton Std Regular</h2>
+      <h2 className='cicle-italic'>Cicle_Fina_Italic</h2>
+      <h2 className='cicle-fina'>Cicle_Fina</h2>
+      <h2 className='cicle-gordita-italic'>Cicle_Gordita_Italic</h2>
+      <h2 className='cicle-gordita'>Cicle_Gordita</h2>
+      <h2 className='cicle-semi-italic'>Cicle_Semi_Italic</h2>
+      <h2 className='cicle-semi'>Cicle_Semi</h2>
+      <h2 className='cosi'>Cosi Azure</h2>
+      <h2 className='dekar-light'>Dekar Light</h2>
+      <h2 className='dekar'>Dekar</h2>
+      <h2 className='neuemontreal-bold'>NeueMontreal-Bold</h2>
+      <h2 className='neuemontreal-bolditalic'>NeueMontreal-BoldItalic</h2>
+      <h2 className='neuemontreal-italic'>NeueMontreal-Italic</h2>
+      <h2 className='neuemontreal-light'>NeueMontreal-Light</h2>
+      <h2 className='neuemontreal-lightitalic'>NeueMontreal-LightItalic</h2>
+      <h2 className='neuemontreal-medium'>NeueMontreal-Medium</h2>
+      <h2 className='neuemontreal-mediumitalic'>NeueMontreal-MediumItalic</h2>
+      <h2 className='neuemontreal-regular'>NeueMontreal-Regular</h2>
+    </di>
   );
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,5 @@
+function Home() {
+  return <h1>asri</h1>;
+}
+
+export default Home;
